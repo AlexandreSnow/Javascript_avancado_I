b59a9437177158ad55d15a62132a47fc1930cf1d
AlexandreSnow <alexandre.souza64@fatec.sp.gov.br>
2019-07-25 22:31:38 -0300

    Aula 5

--- javascript-avancado-ii/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-ii/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,39 +3,51 @@ class NegociacaoController {
     constructor() {
         
         let $ = document.querySelector.bind(document);
+        
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
+         
         this._listaNegociacoes = new Bind(
-            new ListaNegociacoes(),
-            new NegociacoesView($('#negociacoesView')),
-            'adiciona','esvazia'
-        );
-        
+            new ListaNegociacoes(), 
+            new NegociacoesView($('#negociacoesView')), 
+            'adiciona', 'esvazia');
+       
         this._mensagem = new Bind(
-            new Mensagem(),
-            new MensagemView($('#mensagemView')),
-            'texto'
-        );
-        
+            new Mensagem(), new MensagemView($('#mensagemView')),
+            'texto');       
     }
     
     adiciona(event) {
+        
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagem.texto = 'Negociação adicionada com sucesso'; 
         this._limpaFormulario();   
     }
-
+    
     importaNegociacoes() {
-        let xhr = new XMLHttpRequest();
+        
+        let service = new NegociacaoService();
+        
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            
+            if(erro) {
+                this._mensagem.texto = erro;
+                return;
+            }
+            
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações importadas com sucesso';
+            
+        });
 
-        xhr.open('GET')
+       
     }
     
     apaga() {
-        this._listaNegociacoes.esvazia();   
+        
+        this._listaNegociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
     }
     
@@ -54,5 +66,4 @@ class NegociacaoController {
         this._inputValor.value = 0.0;
         this._inputData.focus();   
     }
-
 }
