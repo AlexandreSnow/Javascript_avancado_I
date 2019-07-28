848627a455ab9498ff16987a49b11e630bd8cc44
AlexandreSnow <alexandre.souza64@fatec.sp.gov.br>
2019-07-28 19:33:39 -0300

    Tela de cadastro de autores

---
+++ ReactJs/cdc-admin/src/App.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import "./App.css";
+import "./css/pure-min.css";
+import "./css/side-menu.css";
+import $ from "jquery";
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {lista: []};
+  }
+
+  componentWillMount() {
+    $.ajax({
+      url:'http://localhost:8080/api/autores',
+      dataType: 'json',
+      success: resposta => {
+        this.setState({lista: resposta});
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div id="layout">
+        <a href="#menu" id="menuLink" class="menu-link">
+          <span />
+        </a>
+        <div id="menu">
+          <div class="pure-menu">
+            <a class="pure-menu-heading" href="#">
+              Company
+            </a>
+            <ul class="pure-menu-list">
+              <li class="pure-menu-item">
+                <a href="#" class="pure-menu-link">
+                  Home
+                </a>
+              </li>
+              <li class="pure-menu-item">
+                <a href="#" class="pure-menu-link">
+                  Autor
+                </a>
+              </li>
+              <li class="pure-menu-item">
+                <a href="#" class="pure-menu-link">
+                  Livro
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div id="main">
+          <div className="header">
+            <h1>Cadastro de Autores</h1>
+          </div>
+          <div className="content" id="content">
+            <div className="pure-form pure-form-aligned">
+              <form className="pure-form pure-form-aligned">
+                <div className="pure-control-group">
+                  <label htmlFor="nome">Nome</label>
+                  <input id="nome" type="text" name="nome" value="" />
+                </div>
+                <div className="pure-control-group">
+                  <label htmlFor="email">Email</label>
+                  <input id="email" type="email" name="email" value="" />
+                </div>
+                <div className="pure-control-group">
+                  <label htmlFor="senha">Senha</label>
+                  <input id="senha" type="password" name="senha" />
+                </div>
+                <div className="pure-control-group">
+                  <label />
+                  <button
+                    type="submit"
+                    className="pure-button pure-button-primary"
+                  >
+                    Gravar
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div>
+              <table className="pure-table">
+                <thead>
+                  <tr>
+                    <th>Nome</th>
+                    <th>email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    this.state.lista.map(autor => {
+                      return (
+                        <tr>
+                          <td>{autor.nome}</td>
+                          <td>{autor.email}</td>
+                        </tr>
+                      );
+                    })
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
