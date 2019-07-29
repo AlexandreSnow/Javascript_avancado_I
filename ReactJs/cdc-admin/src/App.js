a1bf87ba4d01401aaea4d16b028b15023db2bc4e
V2Saude <alexandre@xbt.com.br>
2019-07-29 12:47:47 -0300

    Componente Autor

--- ReactJs/cdc-admin/src/App.js
+++ ReactJs/cdc-admin/src/App.js
@@ -1,64 +1,10 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
-import InputCustomizado from './componentes/InputCustomizado';
+import AutorBox from './Autor';
 
 class App extends Component {
 
-  constructor() {
-    super();    
-    this.state = {lista : [],nome:'',email:'',senha:''};
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-    this.setResposta = this.setResposta.bind(this);
-  }
-
-  componentDidMount() {  
-    $.ajax({
-        url:"https://cdc-react.herokuapp.com/api/autores",
-        dataType: 'json',
-        success: resposta => {    
-          this.setResposta(resposta);
-        }
-      } 
-    );          
-  }
-
-  enviaForm(evento) {
-    evento.preventDefault();    
-    $.ajax({
-      url:'https://cdc-react.herokuapp.com/api/autores',
-      contentType:'application/json',
-      dataType:'json',
-      type:'post',
-      data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
-      success: resposta => {
-        this.setResposta(resposta);        
-      },
-      error: resposta => {
-        console.log("erro");
-      }      
-    });
-  }
-
-  setResposta(resposta) {
-    this.setState({lista: resposta.slice(resposta.length-5, resposta.legth)});
-  }
-
-  setNome(evento) {
-    this.setState({nome:evento.target.value});
-  }
-
-  setEmail(evento) {
-    this.setState({email:evento.target.value});
-  }  
-
-  setSenha(evento) {
-    this.setState({senha:evento.target.value});
-  }
   render() {    
     return (
       <div id="layout">
@@ -87,40 +33,7 @@ class App extends Component {
                     <h1>Cadastro de Autores</h1>
                   </div>
                   <div className="content" id="content">
-                    <div className="pure-form pure-form-aligned">
-                      <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-                        <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>                                              
-                        <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>                                              
-                        <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>                                                                      
-                        <div className="pure-control-group">                                  
-                          <label></label> 
-                          <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                        </div>
-                      </form>             
-
-                    </div>  
-                    <div>            
-                      <table className="pure-table">
-                        <thead>
-                          <tr>
-                            <th>Nome</th>
-                            <th>email</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {
-                            this.state.lista.map(function(autor){
-                              return (
-                                <tr key={autor.id}>
-                                  <td>{autor.nome}</td>
-                                  <td>{autor.email}</td>
-                                </tr>
-                              );
-                            })
-                          }
-                        </tbody>
-                      </table> 
-                    </div>             
+                    <AutorBox/>
                   </div>
                 </div>            
 
